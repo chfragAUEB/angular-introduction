052a02eceee489e0f22813e8c47d45e4fb022c62
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2023-03-28 09:37:05 +0300

    before table component

--- src/app/component-input/component-input.component.ts
+++ src/app/component-input/component-input.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+
 @Component({
   selector: 'app-component-input',
   templateUrl: './component-input.component.html',
-  styleUrls: ['./component-input.component.css']
+  styleUrls: ['./component-input.component.css'],
 })
 export class ComponentInputComponent {
-
+  userData: User[] = [
+    { firstName: 'John', lastName: 'Doe', age: 20 },
+    { firstName: 'Jane', lastName: 'Smith', age: 19 },
+    { firstName: 'Joe', lastName: 'Schmoe', age: 33 },
+    { firstName: 'Jane', lastName: 'Brown', age: 44 },
+  ];
 }
