93eb99ae2c379de8697440ec0fed1ce442ce422e
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2023-03-28 06:58:17 +0300

    two way bind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { GreetingComponent } from './greeting/greeting.component';
 import { OneWayBindComponent } from './one-way-bind/one-way-bind.component';
 import { EventBindComponent } from './event-bind/event-bind.component';
+import { TwoWayBindComponent } from './two-way-bind/two-way-bind.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GreetingComponent,
     OneWayBindComponent,
-    EventBindComponent
+    EventBindComponent,
+    TwoWayBindComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/two-way-bind/two-way-bind.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two-way-bind',
+  templateUrl: './two-way-bind.component.html',
+  styleUrls: ['./two-way-bind.component.css'],
+})
+export class TwoWayBindComponent {
+  userInput = '';
+
+  resetUserInput() {
+    this.userInput = '';
+  }
+}
