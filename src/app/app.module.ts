c562a5e35732ea1d047d67f111ba4f53f5452d48
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2023-04-08 17:50:34 +0300

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { GreetingComponent } from './greeting/greeting.component';
-import { OneWayBindComponent } from './one-way-bind/one-way-bind.component';
-import { EventBindComponent } from './event-bind/event-bind.component';
-import { TwoWayBindComponent } from './two-way-bind/two-way-bind.component';
+
 import { TemplateVariablesComponent } from './template-variables/template-variables.component';
 import { StructuralDirectivesComponent } from './structural-directives/structural-directives.component';
 import { ComponentInputComponent } from './component-input/component-input.component';
@@ -24,9 +22,13 @@ import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.componen
 
 const routes: Routes = [
   { path: 'greeting', component: GreetingComponent },
-  { path: 'one-way-bind', component: OneWayBindComponent },
-  { path: 'event-bind', component: EventBindComponent },
-  { path: 'two-way-bind', component: TwoWayBindComponent },
+  {
+    path: 'bind-examples',
+    loadChildren: () =>
+      import('./bind-examples-module/bind-examples.module').then(
+        (m) => m.BindExamplesModule
+      ),
+  },
   { path: 'structural-directives', component: StructuralDirectivesComponent },
   { path: 'template-variables', component: TemplateVariablesComponent },
   { path: 'component-input', component: ComponentInputComponent },
@@ -42,9 +44,6 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     GreetingComponent,
-    OneWayBindComponent,
-    EventBindComponent,
-    TwoWayBindComponent,
     TemplateVariablesComponent,
     StructuralDirectivesComponent,
     ComponentInputComponent,
