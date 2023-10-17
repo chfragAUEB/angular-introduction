cf0d201b98f415a1f2e22affb64ae595ec02f244
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2023-10-17 21:14:56 +0300

    list users

--- src/app/crud-demo/list-users/list-users.component.ts
+++ src/app/crud-demo/list-users/list-users.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppService } from 'src/app/app.service';
+import { Person } from 'src/app/interfaces/person';
 
 @Component({
   selector: 'app-list-users',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './list-users.component.html',
-  styleUrls: ['./list-users.component.css']
+  styleUrls: ['./list-users.component.css'],
 })
-export class ListUsersComponent {
+export class ListUsersComponent implements OnInit {
+  users: Person[] = [];
+  constructor(private service: AppService = Inject(AppService)) {}
 
+  ngOnInit(): void {
+    this.service.getAllUsers().subscribe((users) => {
+      this.users = users;
+    });
+  }
 }
