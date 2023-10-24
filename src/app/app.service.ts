806de021090d28a89277b06f3d78bc8c385a4c17
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2023-10-24 21:17:23 +0300

    update user

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable, inject } from '@angular/core';
 import { Person } from './interfaces/person';
 import { DeleteUserComponent } from './crud-demo/crud/delete-user/delete-user.component';
 
+const API = 'http://localhost:3000/users';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -10,11 +12,11 @@ export class AppService {
   constructor(private http: HttpClient = inject(HttpClient)) {}
 
   getAllUsers() {
-    return this.http.get<Person[]>('http://localhost:3000/users');
+    return this.http.get<Person[]>(API);
   }
 
   getUserById(id: number) {
-    return this.http.get<Person>(`http://localhost:3000/users/${id}`);
+    return this.http.get<Person>(`${API}/${id}`);
   }
 
   addUser(user: Person) {
@@ -23,5 +25,10 @@ export class AppService {
 
   deleteUser(id: number) {
     return this.http.delete<Person>(`http://localhost:3000/users/${id}`);
+  }
+
+  updateUser(user: Person) {
+    console.log('SERVICE', user);
+    return this.http.put<Person>(`${API}/${user.id}`, user);
   }
 }
